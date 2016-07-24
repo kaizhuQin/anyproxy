0840cb9940f549da43519f339680fb7d823a9040
kaizhuQin <qinkaizhu@tankaizhudeMac-mini.local>
2016-07-25 00:00:37 +0800

    dev

--- web/detail.js
+++ web/detail.js
@@ -11,11 +11,15 @@ define("./detail",['$', 'gallery/underscore/1.6.0/underscore.js'],function(requi
 		'			    <li><strong>method : </strong><%= method %> </li>'+
 		'			    <li><strong>path : </strong><br/><%= path %> <span></span></li>'+
 		'			    <li><strong>url : <a href="<%= url %>" target="_blank">点击访问</a></strong><br /><%= url %> <span></span></li>'+
+		'			</ul>'+
+		'		</div>'+
+		'	</section>'+
 
-		'			    <li><strong>196测试url : <a href="<%= url %>" target="_blank">点击访问</a></strong><br /><%= url %> <span></span></li>'+
-		'			    <li><strong>预发url : <a href="<%= url %>" target="_blank">点击访问</a></strong><br /><%= url %> <span></span></li>'+
-		'			    <li><strong>线上url : <a href="<%= url %>" target="_blank">点击访问</a></strong><br /><%= url %> <span></span></li>'+
-		'	<% _.each(mySiteDomainLink, function(v,k) { %> <li><strong><%= k %></strong> : <%= v %></li><% }); %>'+
+		'	<section class="req">'+
+		'		<h4 class="subTitle">requestTool</h4>'+
+		'		<div class="detail">'+
+		'			<ul class="uk-list">'+
+		'	<% _.each(mySiteDomainLink, function(v,k) { %> <li><strong><%= k %> : <a href="<%= v %>" target="_blank">点击访问</a></strong> <br/> <%= v %></li><% }); %>'+
 
 		'			</ul>'+
 		'		</div>'+
@@ -61,49 +65,49 @@ define("./detail",['$', 'gallery/underscore/1.6.0/underscore.js'],function(requi
 
 		/**********************config domain***************************/
 		
-		console.log(domainConfig);
+		// console.log(domainConfig);
 		// console.log(data);
 		var mySiteDomain = {};
 		var mySiteDomainLink = {};
 		
-		// var domainConfig = {
-  //           'mDomain':{
-  //               'dev':'m.xueqiu360.net',
-  //               'prerelease':'mtest.gxq168.com',
-  //               'release':'m.gxq168.com'
-  //           },
-  //           'appwebDomain':{
-  //               'dev':'appweb.xueqiu360.net',
-  //               'prerelease':'appwebtest.gxq168.com',
-  //               'release':'appweb.gxq168.com'
-  //           },
-  //           'serviceDomain':{
-  //               'dev':'service.xueqiu360.net',
-  //               'prerelease':'servicetest.gxq168.com',
-  //               'release':'service.gxq168.com'
-  //           }
-  //       };
+		var domainConfig = {
+            'mDomain':{
+                'dev':'m.xueqiu360.net',
+                'prerelease':'mtest.gxq168.com',
+                'release':'m.gxq168.com'
+            },
+            'appwebDomain':{
+                'dev':'appweb.xueqiu360.net',
+                'prerelease':'appwebtest.gxq168.com',
+                'release':'appweb.gxq168.com'
+            },
+            'serviceDomain':{
+                'dev':'service.xueqiu360.net',
+                'prerelease':'servicetest.gxq168.com',
+                'release':'service.gxq168.com'
+            }
+        };
 
-  //       for(var damianName in domainConfig)
-  //       {
-  //       	for (var env in domainConfig[damianName]) 
-  //       	{
-  //   			if(domainConfig[damianName][env] == data.host)
-  //   			{
-  //   				// mySiteDomain ＝ domainConfig[damianName];
-  //   				console.log(domainConfig[damianName]);
-  //   			};
-  //   		};	
-  //       }
+        for(var damianName in domainConfig)
+        {
+        	for (var env in domainConfig[damianName]) 
+        	{
+    			if(domainConfig[damianName][env] == data.host)
+    			{
+    				mySiteDomain=domainConfig[damianName];
+    			};
+    		};	
+        }
 
-  //       if(mySiteDomain.length)
-  //       {
-  //       	for(var env in mySiteDomain){
-  //       		mySiteDomainLink[env] = data.protocol+"://"+mySiteDomain[env]+data.path;
-  //       	}
-  //       }
+        if(mySiteDomain)
+        {
+        	console.log(mySiteDomain);
+        	for(var env in mySiteDomain){
+        		mySiteDomainLink[env] = data.protocol+"://"+mySiteDomain[env]+data.path;
+        	}
+        }
 
-  //       data['mySiteDomainLink'] = mySiteDomainLink;
+        data['mySiteDomainLink'] = mySiteDomainLink;
 		/**********************config domain***************************/
 
 		var $baseTpl = $(_.template(tpl, data));
